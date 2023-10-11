907cecb0aa87b43643d48310322f4c468d4e3c15
Aaron Defazio <adefazio@fb.com>
2023-10-11 09:56:16 -0700

    Headers

--- synthetic_sched_grid.py
+++ synthetic_sched_grid.py
@@ -3,6 +3,7 @@
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+
 import numpy as np
 import scipy
 from scipy.optimize import Bounds, LinearConstraint, minimize, SR1
